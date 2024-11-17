517f1099eb0cae3c9b9b845e0f4fbb94c010f646
Administrator <Administrator@user2>
2024-11-17 14:36:21 +0530

    Commiting the completed assignment

---
+++ src/test/java/pages/ApplicationHomepage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.sun.tools.javac.util.Assert;
+
+public class ApplicationHomepage {
+	
+	@FindBy(xpath="//*[text()='New Article']")
+	WebElement newArticleTab;
+	/*
+	 * @FindBy(id="add-to-cart-sauce-labs-bike-light") WebElement itemTwo;
+	 * 
+	 * @FindBy(id="shopping_cart_container") WebElement cartIcon;
+	 * 
+	 * @FindBy (xpath = "//span[contains(text(),'Products')]") WebElement
+	 * productsTitle;
+	 */
+	
+	public ApplicationHomepage(WebDriver driver){
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	
+	public void validateHomePage() {
+		Assert.check(newArticleTab.isDisplayed());
+	}
+	
+	public void navigateToArticle() {
+		newArticleTab.click();
+	}
+	
+	/*
+	 * public boolean isOnProducts() { return productsTitle.isDisplayed();
+	 * 
+	 * }
+	 */
+
+
+}
